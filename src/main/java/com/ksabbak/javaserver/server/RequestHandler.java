90b8f1acf3f8ea0f62d33bd844b9a3def92ee778
ksabbak <ksabbak@gmail.com>
2018-06-18 16:30:05 -0500

    Clean up

--- src/main/java/com/ksabbak/javaserver/server/RequestHandler.java
+++ src/main/java/com/ksabbak/javaserver/server/RequestHandler.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,44 +13,39 @@ import java.util.List;
 public class RequestHandler implements Runnable{
     private Socket socket;
     private Router router;
-    private Server server;
     private InputStream inputStream;
 
 
-    public RequestHandler(Socket socket, Router router, Server server) {
+    public RequestHandler(Socket socket, Router router) {
         this.socket = socket;
         this.router = router;
-        this.server = server;
-        System.out.println("HELLO");
     }
 
     public void run(){
             socketGetInputStream();
             BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+
             String unparsedHeader = "";
             String line;
             Boolean blankLine = false;
 
-//            server.turnListeningOff();
             while (!blankLine && ((line = readLine(in)) != null)) {
                 unparsedHeader += line + "\n";
                 if (line.trim().isEmpty()) {
                     blankLine = true;
                 }
             }
+
             RequestParser requestParser = new RequestParser(unparsedHeader);
-        System.out.println(":)");
 
         List<Integer> unparsedBody = new ArrayList<Integer>();
             Integer character;
             int length = 0;
 
             while ((length < requestParser.getContentLength()) && ((character = read(in)) != -1)) {
-                System.out.println("?????");
                 unparsedBody.add(character);
                 length++;
             }
-        System.out.println(":(");
 
             requestParser.addBody(unparsedBody);
             Request request = requestParser.parse();
@@ -104,16 +98,6 @@ public class RequestHandler implements Runnable{
             e.printStackTrace();
         }
     }
-
-//    private Socket serverSocketAccept(){
-//        try {
-//            return serverSocket.accept();
-//        } catch (IOException e) {
-//            System.out.println("SERVER SOCKET ACCEPT ERROR");
-//            e.printStackTrace();
-//            return new Socket();
-//        }
-//    }
 
 }
 
--- src/main/java/com/ksabbak/javaserver/server/Server.java
+++ src/main/java/com/ksabbak/javaserver/server/Server.java
@@ -7,60 +7,17 @@ import com.ksabbak.javaserver.router.Router;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Server {
-    Boolean listening = false;
     public void run(int portNumber, Router router) throws IOException {
         Routable routable = new Routes(router);
-        int totalThreads = 10;
-//        int i = 0;
-        ExecutorService executorService = Executors.newFixedThreadPool(totalThreads);
         ServerSocket serverSocket =
                 new ServerSocket(portNumber);
         while (true) {
             Socket socket = serverSocket.accept();
-//                newThread(socket, router, executorService);
-                Runnable requestHandler = new RequestHandler(socket, router, this);
-                Thread thread = new Thread(requestHandler);
-                thread.start();
-//                executorService.execute(requestHandler);
-
-//                RequestHandler requestHandler = new RequestHandler(socket, router, this);
-//            Runnable requestHandler = new RequestHandler(serverSocket, router, this);
-//            executorService.execute(requestHandler);
-
-
-
+            Runnable requestHandler = new RequestHandler(socket, router);
+            Thread thread = new Thread(requestHandler);
+            thread.start();
         }
     }
-
-//    public void newThread(ServerSocket serverSocket, Router router, ExecutorService executorService){
-//        if (!listening) {
-//            Runnable requestHandler = new RequestHandler(serverSocket, router, this);
-//            executorService.execute(requestHandler);
-//            listening = true;
-//        }
-////        requestHandler.run();
-//    }
-
-//    public void turnListeningOff(){
-//        listening = false;
-//    }
-
 }
-
-//public class TestThreadPool {
-//     public static void main(String[] args) {
-//        ExecutorService executor = Executors.newFixedThreadPool(5);//creating a pool of 5 threads
-//        for (int i = 0; i < 10; i++) {
-//            Runnable worker = new WorkerThread("" + i);
-//            executor.execute(worker);//calling execute method of ExecutorService
-//          }
-//        executor.shutdown();
-//        while (!executor.isTerminated()) {   }
-//
-//        System.out.println("Finished all threads");
-//    }
-// }
