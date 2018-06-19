f092ca910324e72b51c9be1e1e6e80bafdfba7a4
ksabbak <ksabbak@gmail.com>
2018-06-19 09:33:13 -0500

    Use thread pool

--- src/main/java/com/ksabbak/javaserver/server/Server.java
+++ src/main/java/com/ksabbak/javaserver/server/Server.java
@@ -7,17 +7,20 @@ import com.ksabbak.javaserver.router.Router;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
     public void run(int portNumber, Router router) throws IOException {
         Routable routable = new Routes(router);
+        int threadPoolSize = 10;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadPoolSize);
         ServerSocket serverSocket =
                 new ServerSocket(portNumber);
         while (true) {
             Socket socket = serverSocket.accept();
             Runnable requestHandler = new RequestHandler(socket, router);
-            Thread thread = new Thread(requestHandler);
-            thread.start();
+            executorService.execute(requestHandler);
         }
     }
 }
